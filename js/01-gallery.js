05ce39abdf24433d8dcd5c3006e9ea1ad6191074
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-06 21:46:09 +0200

    added libraries

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryGroup = document.querySelector('.gallery');
+
+function createGallaryMarkup() {
+    const markup = galleryItems.map(elem => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${elem.original}">
+          <img
+            class="gallery__image"
+            src="${elem.preview}"
+            data-source="${elem.original}"
+            alt="Image description"
+          />
+        </a>
+      </div>`
+    }).join('');
+
+    galleryGroup.innerHTML = markup;
+}
+
+createGallaryMarkup();
+
+galleryGroup.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    const selectedIMG = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${selectedIMG}">
+    `)
+
+    instance.show()
+
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+            instance.close()
+        }
+    });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryGroup = document.querySelector('.gallery');
+
+function createGallaryMarkup() {
+    const markup = galleryItems.map(elem => {
+        return `<a class="gallery__item" href="${elem.original}">
+        <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+      </a>`
+    }).join('');
+
+    galleryGroup.innerHTML = markup;
+}
+
+createGallaryMarkup();
+
+galleryGroup.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+    e.preventDefault();
+    // if (e.target.nodeName !== "IMG") {
+    //     return;
+    // }
+    var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+    console.log(lightbox);
+    lightbox.options.captionsData = 'alt';
+    lightbox.options.captionDelay = 250;
+}
